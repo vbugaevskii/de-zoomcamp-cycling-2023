c7ad3a43c992b6503db23d61bacfbff664b04e0c
Vladimir Bugaevskii <bugaevsky@mail.ru>
2023-04-26 01:16:39 +0300

    Update stg_rides_info

--- dbt/cycling/models/staging/stg_rides_info.sql
+++ dbt/cycling/models/staging/stg_rides_info.sql
@@ -7,42 +7,36 @@
 
 with bike_point_db as (
     select
-        TerminalName as station_id,
-        Lat,
-        Lon
+        Id as station_id,
+        TerminalName as station_id_
     from {{ source('staging', 'bike_point') }}
 
     union all
 
     select
         Id as station_id,
-        Lat,
-        Lon
+        Id as station_id_
     from {{ source('staging', 'bike_point') }}
 )
 
 select
     rental_id,
     bike_id,
-    start_station_id,
+    sbp.station_id as start_station_id,
     start_station_name,
     start_datetime,
-    sbp.Lat as start_station_lat,
-    sbp.Lon as start_station_lon,
-    end_station_id,
+    ebp.station_id as end_station_id,
     end_station_name,
     end_datetime,
-    ebp.Lat as end_station_lat,
-    ebp.Lon as end_station_lon,
     end_datetime - start_datetime as duration
 from (
     select *
     from {{ source('staging', 'usage_stats') }}
 ) as db_raw
 inner join bike_point_db as sbp
-on sbp.station_id = start_station_id
+on sbp.station_id_ = db_raw.start_station_id
 inner join bike_point_db as ebp
-on ebp.station_id = start_station_id
+on ebp.station_id_ = db_raw.end_station_id
 where duration >= 0 and duration <= 86400
 
 -- dbt build --m <model.sql> --var 'is_test_run: false'
